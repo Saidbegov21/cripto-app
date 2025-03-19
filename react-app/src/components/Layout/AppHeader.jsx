03bf57235c9203c637368719d94cc08d725a2b3f
S.R.K. <114253136+Saidbegov21@users.noreply.github.com>
2025-03-20 01:26:26 +0300

    Разработка Header

--- react-app/src/components/Layout/AppHeader.jsx
+++ react-app/src/components/Layout/AppHeader.jsx
@@ -1,16 +1,63 @@
-import { Layout } from 'antd';
+import { Layout, Select, Space, Button } from "antd";
+import { useCrypto } from "../../context/crypto-context";
+import { useEffect, useState } from "react";
 
 const headerStyle = {
-    textAlign: 'center',
-    color: '#fff',
-    height: 64,
-    paddingInline: 48,
-    lineHeight: '60px',
-    backgroundColor: '#4096ff',
-  };
-
-export default function AppHeader(){
-    return(
-        <Layout.Header style={headerStyle}>Header</Layout.Header>
-    )
+  width: "100%",
+  textAlign: "center",
+  height: 60,
+  paddign: "1rem",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
 };
+
+export default function AppHeader() {
+  const [select, setSelect] = useState(false);
+  const [modal, setModal] = useState(false);
+  const { crypto } = useCrypto();
+
+  useEffect(() => {
+    const keypress = (event) => {
+      if (event.key === "/") {
+        setSelect((prew) => !prew);
+      }
+    };
+    document.addEventListener("keypress", keypress);
+    return () => document.removeEventListener("keypress", keypress);
+  }, []);
+
+  function handlSelect(value) {
+    console.log(value);
+  }
+
+  return (
+    <Layout.Header style={headerStyle}>
+      <Select
+        style={{
+          width: 250,
+        }}
+        open={select}
+        onSelect={handlSelect}
+        onClick={() => setSelect((prew) => !prew)}
+        value="plase / to open"
+        options={crypto.map((coin) => ({
+          label: coin.name,
+          value: coin.id,
+          icon: coin.icon,
+        }))}
+        optionRender={(option) => (
+          <Space>
+            <img
+              style={{ width: 20 }}
+              src={option.data.icon}
+              alt={option.data.label}
+            />
+            {option.data.label}
+          </Space>
+        )}
+      />
+      <Button type="primary">Add Asset</Button>
+    </Layout.Header>
+  );
+}
--- react-app/src/context/crypto-context.jsx
+++ react-app/src/context/crypto-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { fetchAssets, fakeFetchCrypto } from "../api";
 import { persentDifference } from "../util";
 
@@ -46,3 +46,7 @@ export function CryptoContextProvider({ children }) {
 }
 
 export default CryptoContext;
+
+export function useCrypto() {
+  return useContext(CryptoContext);
+}
